3a40620f93cca0259a6de6bb0639d0d7ee1fb0fd
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-04 21:54:02 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,13 @@ import { getBreeds, getCats } from '../cat-api';
 
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-import { zIndex } from 'styled-system';
 
 export const App = () => {
 
  const [breeds, setBreeds] = useState([]);
  const [selectedBreed, setSelectedBreed] = useState(null);
  const [cats, setCats] = useState([]);
- const [galleryivisible, setgalleryivisible] = useState(false)
+ const [galleryiVisible, setGalleryiVisible] = useState(false)
 
   useEffect(() => {
     async function fetchBreeds() {
@@ -29,32 +28,35 @@ export const App = () => {
 
     async function fetchCats() {
       if(!selectedBreed) {
-        setgalleryivisible(false);
+        setGalleryiVisible(false);
         return
       }
 
       const data = await getCats(selectedBreed)
       setCats(data);
-      setgalleryivisible(true);
+      setGalleryiVisible(true);
     }
     fetchCats();
-  },
-  [selectedBreed]);
+  }, [selectedBreed]);
 
   const options = breeds.map(breed => ({
     value: breed.id,
     label: breed.name,
-  }))
+  }));
+
+  const handleSelectChange = option => {
+    setSelectedBreed(option.value);
+  }
 
   return (
     <>
   {options.length > 0 && (
-      <Select style={{zIndex: '100'}} options={options} onChange={option => setSelectedBreed(option.value)}>
+      <Select options={options} onMenuOpen ={() => setGalleryiVisible(false)} onChange={handleSelectChange}>
       </Select>
   )} 
 
 
-  {galleryivisible && cats.length > 0 && (
+  {galleryiVisible && cats.length > 0 && (
     <div style={{maxWidth: '100%', margin: '0 auto' }}>
          <ImageGallery 
             items={cats.map(cat => ({
