05ccafc1041aafcdedcf4fe452805eff1730e9b0
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-04 19:37:45 +0300

    sum

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,17 @@
-// import axios from 'axios';
+import axios from "axios";
 
-// axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
-// axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_CAT_API_KEY;
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_CAT_API_KEY;
 
-// export const getCats = async breed => {
-//   const response = await axios.get(`/images/search?breed_ids=${breed}`);
-//   return response.data;
-// };
+export const getCats = async (breed) => {
+  const res = await axios.get(
+    `/images/search?breed_ids=${breed}&limit=10`, 
+  );
+ return res.data;
+}
 
-// export const getBreeds = async () => {
-//   const response = await axios.get('/breeds');
-//   return response.data;
-// };
+export const getBreeds = async () => {
+  const res = await axios.get('/breeds', 
+);
+  return res.data
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,81 +1,13 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import Select from 'react-select';
 import { GlobalStyle } from './GlobalStyle';
-// import { getBreeds, getCats } from '../cat-api';
-// import { StickerList } from './StickerList';
-// import stickers from '../stickers.json';
-
-// export const App = () => {
-//   const [breeds, setBreeds] = useState([]);
-//   const [selectedBreed, setSelectedBreed] = useState(null);
-//   const [cat, setCat] = useState(null);
-
-//   useEffect(() => {
-//     async function fetchBreeds() {
-//       const data = await getBreeds();
-//       setBreeds(data);
-//     }
-//     fetchBreeds();
-//   }, []);
-
-//   const options = breeds.map(breed => ({
-//     value: breed.id,
-//     label: breed.name,
-//   }));
-
-//   useEffect(() => {
-//     if (selectedBreed === null) return;
-
-//     async function fetchCats() {
-//       const data = await getCats(selectedBreed);
-//       setCat(data[0]);
-//     }
-//     fetchCats();
-//   }, [selectedBreed]);
-
-//   return (
-//     <>
-//       {options.length > 0 && (
-//         <Select
-//           options={options}
-//           onChange={option => setSelectedBreed(option.value)}
-//         />
-//       )}
-//       {cat && (
-//         <div>
-//           <img src={cat.url} alt="" width="320" />
-//         </div>
-//       )}
-//       {/* <StickerList stickers={stickers} /> */}
-//       <GlobalStyle />
-//     </>
-//   );
-// };
-
-// const getCats = async () => {
-//   const res = await axios.get(
-//     'https://api.thecatapi.com/v1/images/search?limit=10', 
-//     {
-//       headers: { 'x-api-key': process.env.REACT_APP_CAT_API_KEY },
-//     }
-//   );
-
-//  return res.data;
-// }
-
-const getBreeds = async () => {
-  const res = await axios.get('https://api.thecatapi.com/v1/breeds', 
-  {
-    headers: { 'x-api-key': process.env.REACT_APP_CAT_API_KEY },
-  });
-
-  return res.data
-}
+import { getBreeds, getCats } from '../cat-api';
 
 export const App = () => {
 
  const [breeds, setBreeds] = useState([]);
+ const [selectedBreed, setSelectedBreed] = useState(null);
+ const [cats, setCats] = useState([]);
 
   useEffect(() => {
     async function fetchBreeds() {
@@ -85,28 +17,36 @@ export const App = () => {
 
     fetchBreeds();
   }, 
-  
   []);
+
+  useEffect(() => {
+
+    if (selectedBreed === null) return;
+
+    async function fetchCats() {
+      const data = await getCats(selectedBreed)
+      setCats(data);
+    }
+    fetchCats();
+  },
+  [selectedBreed]);
 
   const options = breeds.map(breed => ({
     value: breed.id,
     label: breed.name,
   }))
 
+  console.log(cats);
 
+  console.log(selectedBreed);
 
   return (
     <>
   {options.length > 0 && (
-      <Select options={options}>
-        {breeds.map(breed => 
-          <option key={breed.id} value={breed.id}>
-            {breed.name}
-          </option> )}
+      <Select options={options} onChange={option => setSelectedBreed(option.value)}>
       </Select>
-  )}
-  {/* {cats.map((cat, idx) => <div key={idx}> <img src={cat.url} alt='' width={320}/></div>)} */}
-
+  )} 
+  {cats && (cats.map((cat, idx) => <div key={idx}> <img src={cat.url} alt='' width={320}/></div>))}
     <GlobalStyle />
     </>
   )
